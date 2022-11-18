ae499216f90062a1bc6dc3421c84c95ec520a22c
haodai <57907386+a596116@users.noreply.github.com>
2022-11-18 16:16:42 +0800

    navbar

---
+++ src/components/navbar/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <van-nav-bar
+    :title="props.title"
+    :left-text="props.leftText"
+    left-arrow
+    @click-left="onClickLeft"
+  />
+</template>
+
+<script setup lang="ts">
+import { CacheEnum } from '@/enum/cacheEnum'
+import router from '@/router'
+import { store } from '@/utils'
+
+const props = withDefaults(
+  defineProps<{
+    title: string
+    leftText?: string
+  }>(),
+  {
+    leftText: '返回',
+  }
+)
+const onClickLeft = () => {
+  history.back()
+}
+</script>
+
+<style scoped></style>
