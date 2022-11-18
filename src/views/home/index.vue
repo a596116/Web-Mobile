7420df24046d5af3b65fda1599bf847f6b6afe84
haodai <57907386+a596116@users.noreply.github.com>
2022-11-18 16:18:35 +0800

    home index

---
+++ src/views/home/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <van-swipe
+    class="w-auto h-[250px] m-5 rounded-lg"
+    :autoplay="3000"
+    lazy-render
+  >
+    <van-swipe-item v-for="image in images" :key="image">
+      <img :src="image" class="w-full h-full" />
+    </van-swipe-item>
+  </van-swipe>
+</template>
+
+<script setup lang="ts">
+const images = [
+  '/img/swipe/bg-1.jpg',
+  '/img/swipe/bg-2.jpg',
+  '/img/swipe/bg-3.jpg',
+  '/img/swipe/bg-4.jpg',
+  '/img/swipe/bg-5.jpg',
+  '/img/swipe/bg-6.jpg',
+]
+const {
+  appContext: {
+    config: {
+      globalProperties: { eventBus },
+    },
+  },
+} = getCurrentInstance()!
+
+onMounted(() => {})
+</script>
+
+<style scoped></style>
